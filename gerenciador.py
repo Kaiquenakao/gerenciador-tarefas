52a31fd55f793bcf4152ed2e51e4ad91178d1466
Kaiquenakao <kaiquenakao@outlook.com>
2021-10-30 11:57:56 -0300

    nome do processador e da arquitetura

--- gerenciador.py
+++ gerenciador.py
@@ -1,12 +1,16 @@
 import PySimpleGUI as sg
 import platform
+import socket
 import psutil
 import time
 
-info = {}
+info = dict()
 info['platform'] = platform.system()
 info['platform-release'] = platform.release()
 info['platform-version'] = platform.version()
+info['architecture'] = platform.machine()
+info['hostname'] = socket.gethostname()
+info['processor'] = platform.processor()
 
 def interface():
     """Layout do gerenciador de tarefas"""
@@ -19,14 +23,17 @@ def interface():
             justification='center')],
         [sg.Text(
             f"Sistema operacional: {info['platform']} {info['platform-release']} - {info['platform-version']}",
-        font=('Arial', 11))]
+        font=('Arial', 11))],
+        [sg.Text(f"Arquitetura: {info['architecture']}",
+                 font=('Arial', 11))],
+        [sg.Text(f"Processador: {info['processor']}",
+                 font=('Arial', 11))]
     ]
-
     return sg.Window(
         "Gerenciador",
         layout=layout,
         finalize=True,
-        size=(400, 200),
+        size=(500, 200),
         grab_anywhere=True
     )
 
@@ -39,5 +46,3 @@ while True:
     if event == sg.WINDOW_CLOSED:
         break
 
-    if event == 'Enviar':
-        sg.Popup("f")
