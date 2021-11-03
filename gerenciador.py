a6356588dd87423a071f098a95e72f07858f3250
Kaiquenakao <kaiquenakao@outlook.com>
2021-11-03 11:54:34 -0300

    memória usada loop

--- gerenciador.py
+++ gerenciador.py
@@ -3,6 +3,7 @@ import platform
 import socket
 import psutil
 import wmi
+import time
 
 
 computer = wmi.WMI()
@@ -19,44 +20,50 @@ info['processor'] = proc_info.Name
 info['ram'] = str(round(psutil.virtual_memory().total / (1024.0 ** 3)))
 info['graphics-card'] = gpu_info.Name
 
+
 estilo = ('Arial', 11)
 
-def interface():
-    """Layout do gerenciador de tarefas"""
-    sg.theme("DarkAmber")
-    layout = [
-        [sg.Text(
-            "Gerenciador de Tarefas",
-            font=('Arial', 13, 'bold'),
-            size=(100,1),
-            justification='center')],
-        [sg.Text(
-            f"Sistema operacional: {info['platform']} {info['platform-release']} - {info['platform-version']}",
-        font=('Arial', 11))],
-        [sg.Text(f"Arquitetura: {info['architecture']}",
-                 font=estilo)],
-        [sg.Text(f"Processador: {info['processor']}",
-                 font=estilo)],
-        [sg.Text(f"Hostname: {info['hostname']}",
-                 font=estilo)],
-        [sg.Text(f"Memória RAM: {info['ram']} GB",
+
+sg.theme("DarkAmber")
+layout = [
+    [sg.Text(
+        "Gerenciador de Tarefas",
+        font=('Arial', 13, 'bold'),
+        size=(100,1),
+        justification='center')],
+    [sg.Text(
+        f"Sistema operacional: {info['platform']} {info['platform-release']} - {info['platform-version']}",
+    font=('Arial', 11))],
+    [sg.Text(f"Arquitetura: {info['architecture']}",
                 font=estilo)],
-        [sg.Text(f"Placa de Vídeo: {info['graphics-card']}",
+    [sg.Text(f"Processador: {info['processor']}",
                 font=estilo)],
-    ]
-    return sg.Window(
-        "Gerenciador",
-        layout=layout,
-        finalize=True,
-        size=(500, 200),
-        grab_anywhere=True
-    )
+    [sg.Text(f"Hostname: {info['hostname']}",
+                font=estilo)],
+    [sg.Text(f"Memória RAM: {info['ram']} GB",
+            font=estilo)],
+    [sg.Text(f"Placa de Vídeo: {info['graphics-card']}",
+            font=estilo)],
+    [sg.Text(f'Memória usada:', 
+            font=estilo),
+    sg.Text(key="memory", size=(5, None), font=estilo)],
+]
+
+window =  sg.Window(
+    "Gerenciador",
+    layout=layout,
+    finalize=True,
+    size=(500, 300),
+    grab_anywhere=True
+)
 
 
-janela = interface()
 
 while True:
-    window, event, value = sg.read_all_windows()
+    event, values = window.read(timeout=100)
+
+
+    window['memory'].update(f'{psutil.virtual_memory()[2]} %')
 
     if event == sg.WINDOW_CLOSED:
         break
